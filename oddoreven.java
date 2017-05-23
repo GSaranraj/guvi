33ae305e5d57e86994a1be7dedc12a93e0cdf8b5
GSaranraj <saranrajganesh99@gmail.com>
2017-05-23 18:38:53 +0530

    Create oddoreven.java

---
+++ oddoreven.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class oddoreven {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner i=new Scanner(System.in);
+int n;
+n=i.nextInt();
+int b;
+b=n%2;
+if(b==0)
+{
+	System.out.println("even");
+	
+}
+else
+{
+	System.out.println("odd");
+}
+
+	}
+
+}
