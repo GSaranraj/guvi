501f58a8173e02bbce80815936a66db1f67c1227
GSaranraj <saranrajganesh99@gmail.com>
2017-05-23 18:23:26 +0530

    Create casese.java

---
+++ casese.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class casese {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner i=new Scanner(System.in);
+		int a;
+		int b;
+		a=i.nextInt();
+		if (a<1)
+		{
+			b=a%100;
+			System.out.println(b);
+		}
+		else
+		{
+			System.out.println(0);
+		}
+	}
+
+}
