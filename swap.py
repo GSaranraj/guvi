2f8a3c51077e560f43d8a1f786c9ff4a34f2b927
GSaranraj <saranrajganesh99@gmail.com>
2017-05-25 14:56:30 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,10 @@
+#swapping of two number without using temprory variable
+a=4
+b=5
+print "before swapping a:",a
+print "before swapping b:",b
+a=a+b
+b=a-b
+a=a-b
+print "After swapping a:",a
+print "after swapping b:",b
