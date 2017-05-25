8754f453b7cc2a9cafe906d67439a37c9f1ae8e1
GSaranraj <saranrajganesh99@gmail.com>
2017-05-25 14:46:57 +0530

    Create primeinterval.py

---
+++ primeinterval.py
@@ -0,0 +1,12 @@
+numb = int(input("Enter a number: "))
+if numb > 1:
+   # check for factors
+   for i in range(2,numb):
+       if (num % i) == 0:
+           print(numb,"is not a prime number")
+           print(i,"times",numb//i,"is",numb)
+           break
+   else:
+       print(numb,"is a prime number") 
+else:
+   print(numb,"is not a prime number")
