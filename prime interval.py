1a1330ce54622d673b18d9b13861feae7ac977ce
GSaranraj <saranrajganesh99@gmail.com>
2017-05-26 08:17:19 -0700

    Update prime interval.py

--- prime interval.py
+++ prime interval.py
@@ -2,7 +2,7 @@ numb = int(input("Enter a number: "))
 if numb > 1:
    # check for factors
    for i in range(2,numb):
-       if (num % i) == 0:
+       if (numb % i) == 0:
            print(numb,"is not a prime number")
            print(i,"times",numb//i,"is",numb)
            break
